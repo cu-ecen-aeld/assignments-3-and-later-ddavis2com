ddf1080c053f8273e1643946473234dd2b4de48b
Donald Lamar Davis II <don@ddavis2.com>
2026-01-19 01:58:44 +0000

    full-test.sh working, changed ret_code to 1 for errors in writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -4,33 +4,36 @@
 #include <errno.h>
 #include <string.h>
 
+#define WRITER_SUCCESS_CODE 0
+#define WRITER_ERROR_CODE 1
+
 int write_data_to_file(const char *filename, const char *data) {
-    int ret_code = 0;
+    int ret_code = WRITER_SUCCESS_CODE;
     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd == -1) {
         syslog(LOG_ERR, "Error opening file for writing: %m");
-        return ret_code = -1;
+        return ret_code = WRITER_ERROR_CODE;
     }
     ssize_t bytes_written = write(fd, data, strlen(data));
     if (bytes_written == -1) {
         syslog(LOG_ERR, "Error writing to file: %m");
-        ret_code = -1;
+        ret_code = WRITER_ERROR_CODE;
     }
     if (close(fd) == -1) {
         syslog(LOG_ERR, "Error closing file: %m");
-        ret_code = -1;
+        ret_code = WRITER_ERROR_CODE;
     }
     return ret_code;
 }
 
 int main(int argc, char *argv[]) {
-    int ret_code = 0;
+    int ret_code = WRITER_SUCCESS_CODE;
     openlog("writer", LOG_USER, LOG_USER);
 
     if (argc < 3) {
         syslog(LOG_ERR, "Usage: %s <file> <text>", argv[0]);
         closelog();
-        return ret_code = -1;
+        return ret_code = WRITER_ERROR_CODE;
     }
 
     const char *filename = argv[1];
@@ -38,7 +41,7 @@ int main(int argc, char *argv[]) {
 
     syslog(LOG_INFO, "Writing %s to %s", text, filename);
     ret_code = write_data_to_file(filename, text);
-    if (ret_code == 0) {
+    if (ret_code == WRITER_SUCCESS_CODE) {
         syslog(LOG_INFO, "Data written to %s successfully.", filename);
     }
 
